c7990fbe9209879b03ae346d5644e836424b2d70
ArtemPedenko <nubostrel@gmail.com>
2026-09-11 09:02:50 +0300

    test+playwrite

---
+++ lua/plugins/playwright.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "thenbe/neotest-playwright",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      adapters = {
+        ["neotest-playwright"] = {
+          options = {
+            persist_project_selection = true,
+            enable_dynamic_test_discovery = true,
+          },
+        },
+      },
+    },
+  },
+}
