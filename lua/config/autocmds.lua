ca107e2434a303038d2fe95fb5165904cc05695c
ArtemPedenko <nubostrel@gmail.com>
2026-09-22 09:55:21 +0300

    цвет выделения

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,6 +18,9 @@ if vim.tbl_contains(vim.v.argv, "--embed") then
     end,
   })
 end
+vim.api.nvim_set_hl(0, "Visual", {
+  bg = "#3d4751",
+})
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
